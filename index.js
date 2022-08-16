8e1c0a8d837ce7d49d2e04b2a62bfc2c4ec75d42
aviash2507 <avinash2632@gmail.com>
2022-08-16 23:56:46 +0530

    background set

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const nextbtn = document.querySelector(".next");
 const prevbtn = document.querySelector(".prev");
 const imagecomp = document.querySelector(".image-container");
 const imagesNum = document.querySelectorAll("img");
+const backImage = document.querySelector("body");
+
 let currentImage = 1;
-let clickOut ;
+let clickOut;
 nextbtn.addEventListener("click", () => {
   currentImage++;
   clearTimeout(clickOut);
   updateImage();
 });
 prevbtn.addEventListener("click", () => {
-    currentImage--;
-    clearTimeout(clickOut); 
+  currentImage--;
+  clearTimeout(clickOut);
   updateImage();
 });
 updateImage();
@@ -19,11 +21,15 @@ function updateImage() {
   if (currentImage > imagesNum.length) {
     currentImage = 1;
   } else if (currentImage < 1) {
-    currentImage = imagesNum.length;  
+    currentImage = imagesNum.length;
   }
   imagecomp.style.transform = `translateX(-${(currentImage - 1) * 500}px)`;
-    clickOut = setTimeout(() => {
+  backImage.style.backgroundImage = `url(${imagesNum[currentImage - 1].src})`;
+  backImage.style.backgroundRepeat = "no-repeat";
+  backImage.style.backgroundSize = "cover";
+
+  clickOut = setTimeout(() => {
     currentImage++;
     updateImage();
-  }, 2000);
+  }, 4000);
 }
